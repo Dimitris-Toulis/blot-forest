382bd76d357705db1d123a3e164cddaa749de6c0
Dimitris-Toulis <dimitritouli@gmail.com>
2024-08-14 20:31:06 +0300

    add changing width to river

--- draw.js
+++ draw.js
@@ -6,16 +6,19 @@ setDocDimensions(width, height);
 const maxRiverAngleTan = 0.2
 const edgeRiverPadding = 10
 
-const river = [];
+const river = [[],[]];
 
 let x = bt.randInRange(0, 125)
-river.push([x, 0])
+let rw = bt.randInRange(3,10)/2
+river[0].push([x-rw, 0])
+river[1].push([x+rw, 0])
 for (let y = 0; y < height;) {
     y += bt.randInRange(7, 10)
     if (y > height) y = height
-        x += bt.randInRange(x - y * maxRiverAngleTan < 0 ? -x + edgeRiverPadding : -y * maxRiverAngleTan,
-                            x + y * maxRiverAngleTan > width ? width - x - edgeRiverPadding : y * maxRiverAngleTan)
-        river.push([x, y])
+        x += bt.randInRange(x - y * maxRiverAngleTan < 0 ? -x+edgeRiverPadding : -y * maxRiverAngleTan,
+                            x + y * maxRiverAngleTan > width ? width - x-edgeRiverPadding : y * maxRiverAngleTan)
+        rw = bt.randInRange(3,10)/2
+        river[0].push([x-rw, y])
+        river[1].push([x+rw, y])
 }
-
-drawLines([bt.catmullRom(river)],{stroke:"blue",width: 5});
+drawLines([bt.catmullRom(river[0]),bt.catmullRom(river[1])]);
